606b6fb82c802c47cde0b5284d9d1a0df5a7869d
DanielGS93 <danielgamboa.80@gmail.com>
2025-06-27 14:26:10 -0400

    proyectofinal 2

---
+++ Proyecto final/stays.js
@@ -0,0 +1,79 @@
+const stays = [
+  {
+    city: "Santiago",
+    country: "Chile",
+    superHost: true,
+    title: "Departamento moderno en Providencia",
+    rating: 4.8,
+    maxGuests: 3,
+    type: "Departamento",
+    beds: 2,
+    photo: "https://imgclasificados3.emol.com/Proyectos/imagenes//PR_FOTO_4629_1.jpg"
+  },
+  {
+    city: "Valparaíso",
+    country: "Chile",
+    superHost: false,
+    title: "Casa bohemia en los cerros",
+    rating: 4.5,
+    maxGuests: 4,
+    type: "Casa completa",
+    beds: 3,
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHH-ZmY4sTUTE0JRYBecP-bIQQL7c-_KZ0zQ&s"
+  },
+  {
+    city: "Pucón",
+    country: "Chile",
+    superHost: true,
+    title: "Cabaña rústica cerca del lago Villarrica",
+    rating: 4.9,
+    maxGuests: 2,
+    type: "Cabaña",
+    beds: 1,
+    photo: "https://images.adsttc.com/media/images/5728/6d8f/e58e/cebb/a100/0201/large_jpg/Casa_Villarrica06.jpg?1462267267"
+  },
+  {
+    city: "Puerto Varas",
+    country: "Chile",
+    superHost: false,
+    title: "Loft frente al lago Llanquihue",
+    rating: 4.7,
+    maxGuests: 2,
+    type: "Loft",
+    beds: 1,
+    photo: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/412635379.jpg?k=90d91eb81fc42c521b23b7cc7ba3dfc42c5903cffee130d661dbb7f6e0bfccff&o=&hp=1"
+  },
+  {
+    city: "La Serena",
+    country: "Chile",
+    superHost: true,
+    title: "Departamento costero con vista al mar",
+    rating: 4.6,
+    maxGuests: 5,
+    type: "Departamento",
+    beds: 3,
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs6XoFnAjjsmby6k_T0pBUj6_B26eJodDygQ&s"
+  },
+  {
+    city: "San Pedro de Atacama",
+    country: "Chile",
+    superHost: false,
+    title: "Hostal típico del desierto",
+    rating: 4.3,
+    maxGuests: 3,
+    type: "Hostal",
+    beds: 2,
+    photo: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/517463023.jpg?k=39665d79a2227d11b7007a2ce07022e6d144e8ed248b944830df8a5821a21841&o=&hp=1"
+  },
+  {
+    city: "Chiloé",
+    country: "Chile",
+    superHost: true,
+    title: "Palafito acogedor en Castro",
+    rating: 4.9,
+    maxGuests: 4,
+    type: "Palafito",
+    beds: 2,
+    photo: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/544599785.jpg?k=5d1a6311424b18431f0a9cec59ad504da3b4da1c1d7baa8965347005d0b01b4a&o=&hp=1"
+  }
+];
