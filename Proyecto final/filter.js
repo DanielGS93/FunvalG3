e48d18bb211d6663839e0d6cb27c934a193766b8
DanielGS93 <danielgamboa.80@gmail.com>
2025-06-27 12:28:23 -0400

    proyecto final

--- Proyecto final/filter.js
+++
@@ -1,8 +0,0 @@
-
-export function filterStays(stays, city, guests) {
-  return stays.filter(stay => {
-    const cityMatch = !city || stay.city === city;
-    const guestsMatch = !guests || stay.maxGuests >= guests;
-    return cityMatch && guestsMatch;
-  });
-}
